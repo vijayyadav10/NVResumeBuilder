4469bb4d1522d05263d9d684efa024eb365988ec
yadav vijay <yadav.vijay@newvisionsoftpune.com>
2021-02-16 13:16:51 +0530

    complete folder structure

---
+++ src/main/java/com/nv/resumebuilder/service/Student.java
@@ -0,0 +1,10 @@
+package com.nv.resumebuilder.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Student {
+	public void display() {
+		System.out.println("i m in school");
+	}
+}
