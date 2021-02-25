58d72e1ca96be1dc261fbd093bd164944c42b448
khushabunewvision <khushabu.warade@newvisionsoftware.in>
2021-02-25 15:35:25 +0530

    fourth commit

--- src/test/java/com/nv/resumebuilder/ResumebuilderApplicationTests.java
+++ src/test/java/com/nv/resumebuilder/ResumebuilderApplicationTests.java
@@ -1,13 +1,27 @@
 package com.nv.resumebuilder;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.nv.resumebuilder.repository.PersonalDetailsRepository;
+import com.nv.resumebuilder.service.PersonalDetailsServices;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ResumebuilderApplicationTests {
-
+class ResumebuilderApplicationTests 
+{
+	@Autowired
+	private PersonalDetailsServices service;
+	@MockBean
+	private PersonalDetailsRepository repository;
 	@Test
-	void contextLoads() {
+	public void savePersonalDetails()
+	{
+		
 	}
 
 }
